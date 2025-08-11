0922fc17eb4d2d89c9f9cdc5c795f392da026bad
Boyboye <theglobalcaleb@gmail.com>
2025-08-11 10:34:18 +0100

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        droseraGreen: '#00ffb2',
+        droseraPurple: '#7a00ff'
+      }
+    }
+  },
+  plugins: [],
+}
